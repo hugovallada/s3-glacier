393f161812a2ec8988f25932f31a73566af88c4c
Hugo Vallada <valladahugo@gmail.com>
2022-10-09 21:20:40 -0300

    Cria o bucket S3 com rules de lifecycle

---
+++ infra/bucket.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "glacier" {
+  bucket = var.bucket_name
+
+  lifecycle_rule {
+    enabled = true
+    prefix  = var.glacier_prefix
+
+    transition {
+      days          = 0
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 1
+      storage_class = "GLACIER"
+    }
+  }
+
+  lifecycle_rule {
+    enabled = true
+    prefix  = "expired/"
+
+    transition {
+      days          = 0
+      storage_class = "DEEP_ARCHIVE"
+    }
+  }
+
+  tags = {
+    "Email" = "valladahugo@gmail.com"
+  }
+
+}
---
+++ infra/variable.tfvars
@@ -0,0 +1,2 @@
+bucket_name = "s3-glacier-buck"
+glacier_prefix = "removed/"
